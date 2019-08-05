42a9c7c327d15c01b4759fc4ac9e92de02382d01
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 14:39:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,13 @@
-argument = [i]
-
 def my_each(argument) # put argument(s) here
   # code here
   if block_given?
     i = 0
 
-    while argument.each do |i|
+    while my_each(argument) do |i|
       yield (argument[i])
       i = i + 1
   end
 
-    argument
   else
       puts "Hey! No block was given"
     end
