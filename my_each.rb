4a4f49ad3786e11ffda770c0697664d885571747
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 14:26:56 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,14 @@
 def my_each(argument) # put argument(s) here
   # code here
-  while
-    yield (argument[i])
-    i = i + 1
-      puts i
+  if block_given?
+    while
+      yield (argument[i])
+      i = i + 1
+        puts i
   end
 
     argument
-
+  else
+      puts "the end"
+    end
 end
