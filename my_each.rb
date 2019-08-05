e38aa95c27d7f662aa745fd64e2ee7f8c03234d1
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 14:21:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
+def my_each(argument) # put argument(s) here
   # code here
+  while
+    yield (argument[i]) 
+  my_each(argument) do |i|
+      puts i
+  end
+
+    argument
+  else
+      puts "the end"
+    end
 end
