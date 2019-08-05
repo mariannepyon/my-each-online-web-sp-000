f11f6040453d30a3b51743fe5f09ea68a5caf992
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 14:40:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -6,9 +6,9 @@ def my_each(argument) # put argument(s) here
     while my_each(argument) do |i|
       yield (argument[i])
       i = i + 1
-  end
+    end
 
   else
       puts "Hey! No block was given"
     end
-end
+  end
